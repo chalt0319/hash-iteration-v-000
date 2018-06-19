1b8d87fa7e54ba0b7df1ba69233e53c5a67cdc77
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-19 19:25:05 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.collect do |kid, age|
+  birthday_kids.each do |kid, age|
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end 
 end
